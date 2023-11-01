faec9b7b44651f4b28c8a184f170e52670a35ee9
TInjusan <jazt72@.com>
2023-11-01 12:28:18 -0700

    props passing data

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,13 @@ const theDescription = reactDescriptions[genRandomInt(2)];
 }
 
 
-function CoreConcept(props){
+function CoreConcept({title, description, image}){
   return(
 
     <li >
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p></p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
