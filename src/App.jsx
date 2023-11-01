4a02ffb5c896212095faef9f854d23164e594ee9
TInjusan <jazt72@.com>
2023-11-01 12:25:10 -0700

    reusable component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImage from "./assets/react-core-concepts.png";
-
+import {CORE_CONCEPTS} from "./data.js";
+ 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -20,12 +21,31 @@ const theDescription = reactDescriptions[genRandomInt(2)];
   );
 }
 
+
+function CoreConcept(props){
+  return(
+
+    <li >
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p></p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts!</h2>
+          <ul> 
+            <CoreConcept title = {CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcept title = {CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept title = {CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+          </ul>
+          </section>
       </main>
     </div>
   );
