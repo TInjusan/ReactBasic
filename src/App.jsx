ffe37ae05ee4672fbf842bd312e29ee7b0496a3d
TInjusan <jazt72@.com>
2024-03-16 13:38:38 -0700

    Displaying component dynamically from set of data (array of objects)

--- src/App.jsx
+++ src/App.jsx
@@ -45,10 +45,13 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts!</h2>
           <ul> 
-            <CoreConcept title = {CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-            <CoreConcept  {...CORE_CONCEPTS[1]}/>
-            <CoreConcept  {...CORE_CONCEPTS[2]}/>
-            <CoreConcept  {...CORE_CONCEPTS[3]}/>
+
+            {
+            // Dynamically display multiple components based on array of objects
+            CORE_CONCEPTS.map((conceptItem)=> <CoreConcept key={conceptItem.title} {...conceptItem}/>)
+            //you must set a key that is unique on each object in the array
+            }
+            
             
           </ul>
           </section>
